621028852e17d1dcc92bfba4cf3621697e2eba65
morez-s <m.sahelgozin@gmail.com>
2024-03-04 22:32:12 +0330

    show race progress

--- src/Classes/Race.php
+++ src/Classes/Race.php
@@ -6,10 +6,26 @@ class Race
 {
     public RaceSetting $setting;
 
-    public function setup(): RaceSetting
+    public function start(): void
+    {
+        $this->setup();
+        $this->progress();
+        // $this->finish();
+    }
+
+    private function setup(): void
     {
         // create the race settings object
         $this->setting = new RaceSetting();
-        return $this->setting;
+    }
+
+    private function progress(): void
+    {
+        // show race progress
+        system('clear');
+        \cli\out("\n#####  Race Started  #####\n");
+        \cli\out("\nPlayer One: " . $this->setting->first_player . "\t\tPlayer Two: " . $this->setting->second_player . "\n\n");
+        test_notify(new \cli\progress\Bar("Race is going on :-)", 100), 100, 100000);
+        \cli\out("\n#####  Race Finished  #####\n");
     }
 }
